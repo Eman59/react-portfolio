c852511e6ced611b0b550f2b03356fb0ad29ae73
Imran <sayyedeman59@gmail.com>
2021-10-08 12:21:17 +0530

    Add all my files

---
+++ src/Components/ImageSection.js
@@ -0,0 +1,93 @@
+import React from "react";
+import styled from "styled-components";
+import resume from "../img/resume.jpg";
+import PrimaryButton from "./PrimaryButton";
+
+function ImageSection() {
+  return (
+    <ImageSectionStyled>
+      <div className="left-content">
+        <img src={resume} alt="" />
+      </div>
+      <div className="right-content">
+        <h4>
+          I am <span>Imran Sayyed</span>
+        </h4>
+        <p className="paragraph">
+          Passionate front-end web developer with experience in using
+          JavaScript, HTML5, and CSS to build all aspects of the user experience
+          and user interface for client-facing landing pages. Specializes in
+          using React JS to build e-commerce sites.
+        </p>
+        <div className="about-info">
+          <div className="info-title">
+            <p>Full Name</p>
+            <p>Age</p>
+            <p>Nationality </p>
+            <p>Languages </p>
+            <p>Location</p>
+            <p>Service</p>
+          </div>
+          <div className="info">
+            <p>: Sayyed Imran</p>
+            <p>: 22</p>
+            <p>: Indian </p>
+            <p>: English, Hindi </p>
+            <p>: Mumbai, Maharashtra</p>
+            <p>: Software Developer</p>
+          </div>
+        </div>
+        <PrimaryButton title={"Download Cv"} />
+      </div>
+    </ImageSectionStyled>
+  );
+}
+
+const ImageSectionStyled = styled.div`
+  margin-top: 5rem;
+  display: flex;
+  @media screen and (max-width: 1000px) {
+    flex-direction: column;
+    .left-content {
+      margin-bottom: 2rem;
+    }
+  }
+  .left-content {
+    width: 100%;
+    img {
+      width: 95%;
+      object-fit: cover;
+      border: 8px solid var(--border-color);
+    }
+  }
+  .right-content {
+    width: 100%;
+    h4 {
+      font-size: 2rem;
+      color: var(--white-color);
+      span {
+        font-size: 2rem;
+      }
+    }
+    .paragraph {
+      padding: 1rem 0;
+    }
+    .about-info {
+      display: flex;
+      padding-bottom: 1.4rem;
+      .info-title {
+        padding-right: 3rem;
+        p {
+          font-weight: 600;
+        }
+      }
+      .info-title,
+      .info {
+        p {
+          padding: 0.3rem 0;
+        }
+      }
+    }
+  }
+`;
+export default ImageSection;
---
+++ src/Pages/HomePage.js
@@ -0,0 +1,108 @@
+import React from "react";
+import styled from "styled-components";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import GithubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import Particle from "../Components/Particle";
+
+function HomePage() {
+  return (
+    <HomePageStyled>
+      <div className="particle-con">
+        <Particle />
+      </div>
+      <div className="typography">
+        <h1>
+          Hi, I'm <span>Imran Sayyed</span>
+        </h1>
+        <p>
+          Proficient in translating designs & wireframes into high-quality code,
+          and writing application interface code via Javascript and ReactJS
+          workflows. Adept at monitoring & maintaining frontend performance and
+          troubleshooting & debugging the same to bolster overall performance.
+        </p>
+        <div className="icons">
+          <a
+            href="https://www.instagram.com/iamimransayyed/"
+            target="_blank"
+            rel="noreferrer"
+            className="icon i-instagram"
+          >
+            <InstagramIcon />
+          </a>
+          <a
+            href="https://github.com/Eman59"
+            target="_blank"
+            rel="noreferrer"
+            className="icon i-github"
+          >
+            <GithubIcon />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/imran-sayyed-6a5b12209/"
+            target="_blank"
+            rel="noreferrer"
+            className="icon i-linkedin"
+          >
+            <LinkedInIcon />
+          </a>
+        </div>
+      </div>
+    </HomePageStyled>
+  );
+}
+
+const HomePageStyled = styled.header`
+  width: 100%;
+  height: 100vh;
+  position: relative;
+
+  .typography {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    width: 80%;
+
+    .icons {
+      display: flex;
+      justify-content: center;
+      margin-top: 1rem;
+      .icon {
+        border: 2px solid var(--border-color);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 50%;
+        transition: all 0.4s ease-in-out;
+        cursor: pointer;
+        &:hover {
+          border: 2px solid #bc2a8d;
+          color: #bc2a8d;
+        }
+        &:not(:last-child) {
+          margin-right: 1rem;
+        }
+        svg {
+          margin: 0.5rem;
+        }
+      }
+
+      .i-linkedin {
+        &:hover {
+          border: 2px solid #0077b5;
+          color: #0077b5;
+        }
+      }
+      .i-github {
+        &:hover {
+          border: 2px solid #5f4687;
+          color: #5f4687;
+        }
+      }
+    }
+  }
+`;
+
+export default HomePage;
