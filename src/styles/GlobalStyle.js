25046b3167b5eb82fbdba4a54b5fe001ea60427f
Eman59 <sayyedeman59@gmail.com>
2022-04-23 15:47:26 +0530

    reverted to normal

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -64,7 +64,7 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     list-style: none;
     text-decoration: none;
-    font-family: 'Baskervville', serif !important;
+    font-family: 'Nunito', sans-serif;
     font-size: 1.1rem;
     
 }
